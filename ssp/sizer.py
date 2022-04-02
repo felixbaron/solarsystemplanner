f1f631b1ef5ed16e4289fb16853a0941e1c6743f
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2022-04-02 16:51:51 +0200

    add more result attributes

--- ssp/sizer.py
+++ ssp/sizer.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 
-def estimate(units, start_date, end_date):
+def estimate(units, start_date, end_date, watt_production_capacity=0.0045):
     """
     Returns size of system
 
@@ -12,19 +12,25 @@ def estimate(units, start_date, end_date):
         consumption on the first day of the energy bill
     end_date : datetime.date object
         consumption on the last day of the energy bill
+    watt_production_capacity : float, optional
+        Rule of thumb: 1 Watt produces 0.0045 units per day (varies as per size, check for the irradiance map)
     Returns
     -------
-    sysetm : size
-        Sysetm size
+    system : size
+        Sysetm size in required kW
     
     >>> estimate(778,"2021/4/9","2022/5/3")
-    2.0
+    {'system_size': 444.44444444444446, 'kwh_per_day': 2.0, 'kwh_per_year': 730.0}
     """
     date_format = "%Y/%m/%d"
     start_date = datetime.strptime(start_date, date_format)
     end_date = datetime.strptime(end_date, date_format)
     delta_days = (end_date - start_date).days
-    return units/delta_days
+    return {
+        "system_size": units/delta_days/watt_production_capacity,
+        "kwh_per_day": units/delta_days,
+        "kwh_per_year": units/delta_days * 365
+    }
 
 if __name__ == "__main__":
     import doctest
