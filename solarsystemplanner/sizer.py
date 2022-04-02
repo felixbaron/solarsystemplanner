a5846db7c96b1203c94e68b535d610f8c179d6af
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2022-04-02 14:37:36 +0200

    initial commit

---
+++ solarsystemplanner/sizer.py
@@ -0,0 +1,15 @@
+def hello():
+    """
+    Returns hello
+
+    Parameters
+    ----------
+    size : {number of power units, int}
+        number of consumed energy
+    
+    Returns
+    -------
+    sysetm : size
+        Sysetm size
+    """
+    return "hello"
