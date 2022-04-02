821f8409e7bdd1822932ffabf98bfbe9e0a42ae1
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2022-04-02 17:19:34 +0200

    implement roof_assessment

--- ssp/roof_assessment.py
+++ ssp/roof_assessment.py
@@ -1,2 +1,24 @@
-def roof_size(system_size):
-    return 73
+def roof_size(system_size, sqm_per_kw_needed=7.0):
+    """
+    Retunrs an estimate of the required roof size for a given system size
+    
+    Parameters
+    ----------
+    system_size : float
+        The size of the photovoltaic system in Watt
+    sqm_per_kw_needed : float
+        The required sq. m per kW need. Rule of thumb is 1 kW requires 6-8 sq. m
+    
+    Returns : float
+        The required roof size to place the PV system
+    
+    Example
+    -------
+    >>> roof_size(3522.34)
+    24.656380000000002
+    """
+    return system_size/1000 * sqm_per_kw_needed
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- ssp/tests.py
+++ ssp/tests.py
@@ -3,4 +3,5 @@ import doctest
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite("sizer"))
+    tests.addTests(doctest.DocTestSuite("roof_assessment"))
     return tests
