84f77a9dbfc1c384228b03fef8b17d212825b922
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2022-04-02 16:26:33 +0200

    update sizer

--- ssp/sizer.py
+++ ssp/sizer.py
@@ -1,15 +1,31 @@
-def estimate(a,b,c):
+from datetime import datetime, date
+
+def estimate(units, start_date, end_date):
     """
     Returns size of system
 
     Parameters
     ----------
-    size : 
-        number of consumed energy
-    
+    units : int
+        consumed units in a give time frame
+    start_date : datetime.date object
+        consumption on the first day of the energy bill
+    end_date : datetime.date object
+        consumption on the last day of the energy bill
     Returns
     -------
     sysetm : size
         Sysetm size
+    
+    >>> estimate(778,"2021/4/9","2022/5/3")
+    2.0
     """
-    return 9
+    date_format = "%Y/%m/%d"
+    start_date = datetime.strptime(start_date, date_format)
+    end_date = datetime.strptime(end_date, date_format)
+    delta_days = (end_date - start_date).days
+    return units/delta_days
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
